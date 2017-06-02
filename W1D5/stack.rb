243189facf91ad14fabf83e45ed770f882d5bf23
boothandrewd <boothandrewd@gmail.com>
2017-06-01 21:22:42 -0700

    Implement stack

---
+++ W1D5/stack.rb
@@ -0,0 +1,19 @@
+# stack.rb
+
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack.push(el)
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack
+  end
+end
