fb57654e927d6534b15ac7c52595399a2b9b3212
boothandrewd <boothandrewd@gmail.com>
2017-06-01 21:29:19 -0700

    Implement queue

---
+++ W1D5/queue.rb
@@ -0,0 +1,19 @@
+# W1D5/queue.rb
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.pop
+  end
+
+  def show
+    @queue
+  end
+end
