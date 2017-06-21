fd148be3ad5ff985d72497992bf0f3369f85851b
boothandrewd <boothandrewd@gmail.com>
2017-06-21 01:15:03 -0700

    Complete auth homework

---
+++ W4D3/AuthHomework/app/models/user.rb
@@ -0,0 +1,46 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :session_token, presence: true, uniqueness: true
+  validates_length_of :password, in: 6..30, allow_nil: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16 * 3 / 4)
+  end
+
+  def reset_session_token!
+    session_token = self.class.generate_session_token
+    save!
+    session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+end
