787bde19712935f3ba2df08a4f57964baf4662d7
boothandrewd <boothandrewd@gmail.com>
2017-07-11 23:19:16 -0700

    Get giphy search working

--- W7D3/skeleton/components/giphys_search.jsx
+++ W7D3/skeleton/components/giphys_search.jsx
@@ -1,3 +1,47 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ searchTerm: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.fetchSearchGiphys(this.state.searchTerm);
+
+    this.setState({ searchTerm: '' });
+  }
+
+  render() {
+    const { handleChange, handleSubmit } = this;
+    const searchTerm = this.state.searchTerm;
+
+    return (
+      <div>
+        <h3>Find GIFs</h3>
+        <form>
+          <label>Search:
+            <input onChange={handleChange} value={searchTerm} />
+          </label>
+
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default GiphysSearch;
--- W7D3/skeleton/giphy_search.jsx
+++ W7D3/skeleton/giphy_search.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { receiveSearchGiphys } from './actions/giphy_actions';
-import { fetchSearchGiphys } from './actions/giphy_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+
+  window.store = store;   // TODO: Remove store from window
 });
